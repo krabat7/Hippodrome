1f59a996f9549c815c10027271e76eba0cfc0e55
Simon <simon.cheremisin@gmail.com>
2023-08-30 17:28:37 +0300

    Добавлен тестовый класс для проверки времени работы метода main класса Main.

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class MainTest {
+    @Test
+    @Timeout(22)
+    @Disabled
+    public void testMainOutput() throws Exception {
+        Main.main(new String[0]);
+    }
+}
